bad724cea0a5e4e5fec62fc18d6706c3304b8d73
iPadisthin <albert@Alberts-MacBook-Pro.local>
2018-06-25 09:44:20 +1000

    lots more

--- js/main.js
+++ js/main.js
@@ -1,41 +1,11 @@
-function openMenu(){
-    $('.overlay').fadeIn();
-    $('nav').animate({
-        marginRight: '0px'
-        }, 500, function() {
-            //animation complete
-    });
-}
-function closeMenu(){
-    $('.overlay').fadeOut();
-    $('nav').animate({
-        marginRight: '-200px'
-        }, 500, function() {
-            //animation complete
-    });
-}
 $(document).ready(function() {
-    /*$('nav').animate({
-            marginRight: '-200px'
-        }, 1000, function() {
-            //animation complete
-    });
-    $('.menu-toggle').click(function(event) {
-        event.preventDefault();
-        if($('nav').css('marginRight') == '-200px'){
-            openMenu();
-        } else {
-            closeMenu();
-        }
-    });
-    $('.overlay').click(function() {
-        closeMenu();
-    });*/
     Barba.Prefetch.init();
     Barba.Pjax.start();
 });
 $(window).scroll(function() {
-    if( $(this).scrollTop() > 250 )  {
+    var offset = $('.main-nav').offset();
+    var offsetTrigger = offset.top + 56;
+    if( $(this).scrollTop() > offsetTrigger )  {
         $('.scroll-nav').addClass('show');
     } 
     else {
