aa66c67b02a60824a7c16702634dd47570854c21
iPadisthin <albert.penticoss@gmail.com>
2018-09-04 14:46:44 +1000

    updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-    Barba.Prefetch.init();
-    Barba.Pjax.start();
+  Barba.Prefetch.init();
+  Barba.Pjax.start();
 });
 $(window).scroll(function() {
-    var offset = $('.main-nav').offset();
-    var offsetTrigger = offset.top + 56;
-    if( $(this).scrollTop() > offsetTrigger )  {
-        $('.scroll-nav').addClass('show');
-    } 
-    else {
-        $('.scroll-nav').removeClass('show');
-    }
+  var offset = $('.main-nav').offset();
+  var offsetTrigger = offset.top + 56;
+  if( $(this).scrollTop() > offsetTrigger )  {
+    $('.scroll-nav').addClass('show');
+  }
+  else {
+    $('.scroll-nav').removeClass('show');
+  }
 });
